d22f91602ca1d04f39de8daf37dd72d3a66ca095
shoyonp <shoyonkumar143@gmail.com>
2024-10-07 22:12:23 +0600

    processing

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log("hello");
 
 const removeActiveClass = () => {
-    const buttons = document.getElementsByClassName("category-btn")
-    console.log(buttons);
-    for(let btn of buttons){
-        btn.classList.remove("active");
-    }
-}
+  const buttons = document.getElementsByClassName("category-btn");
+  console.log(buttons);
+  for (let btn of buttons) {
+    btn.classList.remove("active");
+  }
+};
 
 // load categories
 const loadCategories = () => {
@@ -33,11 +33,89 @@ const loadCategoryPets = (category) => {
       .then((data) => {
         removeActiveClass();
         const activeBtn = document.getElementById(`btn-${category}`);
-        activeBtn.classList.add("active")
-        displayAllPets(data.data)
+        activeBtn.classList.add("active");
+        displayAllPets(data.data);
       })
       .catch((error) => console.log(error));
   }, 2000);
+};
+
+// load pet details
+const loadPetDetails = (petId) => {
+  fetch(`https://openapi.programming-hero.com/api/peddy/pet/${petId}`)
+    .then((res) => res.json())
+    .then((data) => displayPetDetails(data.petData))
+    .catch((error) => console.log(error));
+};
+
+// load pet Photos
+const loadPetPhotos = (photos) => {
+  fetch(`https://openapi.programming-hero.com/api/peddy/pet/${photos}`)
+    .then((res) => res.json())
+    .then((data) => displayPetPhotos(data.petData.image))
+    .catch((error) => console.log(error));
+};
+
+// countdown modal
+const openModal = () => {
+  my_modal_2.showModal();
+
+  let countdown = 3;
+
+  // setInterval(() => {
+  //   countdown--;
+  //   if(countdown === 0){
+  //    let value = document.getElementById("countdownValue")
+  //    value.innerText = countdown;
+  //    const div = document.getElementById("countdown-modal");
+  //    div.style.display = "none";
+
+  //   }
+  // },1000)
+};
+
+// display pet photos
+const displayPetPhotos = (photos) => {
+  const petPhoto = document.getElementById("photos");
+  const div = document.createElement("div");
+
+  div.innerHTML = `
+  <div class=" p-3 grid grid-cols-2"><img class="rounded-lg" src="${photos}"></div>
+  `;
+  petPhoto.append(div);
+};
+
+// display pet details
+const displayPetDetails = (petDetails) => {
+  const details = document.getElementById("modal-content");
+  document.getElementById("my_modal_5").showModal();
+  details.innerHTML = `
+  <img class="rounded-lg mb-3 w-full" src="${petDetails.image}">
+  <h1 class="font-bold text-2xl mb-3" >${petDetails.pet_name}</h1>
+ <div class="grid grid-cols-2 gap-2 mb-3">
+ <p class="flex items-center text-gray-500 gap-2"><img src="https://img.icons8.com/?size=16&id=68585&format=png"> Breed: ${
+   petDetails.breed ? petDetails.breed : "Not available"
+ }</p>
+    <p class="flex items-center text-gray-500 gap-2"><img class="w-4" src="https://img.icons8.com/?size=24&id=84997&format=png"> Birth: ${
+      petDetails.date_of_birth ? petDetails.date_of_birth : "Not available"
+    }</p>
+    <p class="flex items-center text-gray-500 gap-2"><img class="w-4" src="https://img.icons8.com/?size=32&id=16275&format=png"> Gender: ${
+      petDetails.gender ? petDetails.gender : "Not available"
+    }</p>
+    <p class="flex items-center text-gray-500 gap-2"><img class="w-4" src="https://img.icons8.com/?size=24&id=85782&format=png"> Price: ${
+      petDetails.price
+    }$ </p>
+    <p class="flex items-center text-gray-500 gap-2 mb-3><img class="w-4" src="https://img.icons8.com/?size=32&id=16275&format=png"> Vaccinated status: ${
+      petDetails.vaccinated_status
+        ? petDetails.vaccinated_status
+        : "Not available"
+    }</p>
+    </div>
+    <hr>
+    <div class="mt-3"><h2 class="font-semibold text-base mb-3">Details Information</h2>
+    <p class="text-gray-500">${petDetails.pet_details}</p>
+    </div>
+  `;
 };
 
 // display all pets
@@ -45,12 +123,8 @@ const displayAllPets = (pets) => {
   const petContainer = document.getElementById("petCard");
   petContainer.innerHTML = "";
 
-  setTimeout(function () {
-    
-  })
-  
   if (pets.length == 0) {
-    petContainer.classList.remove("grid")
+    petContainer.classList.remove("grid");
     petContainer.innerHTML = `
      <div>
      <div class="bg-gray-200 border-none rounded-3xl text-center p-8 ">
@@ -64,7 +138,7 @@ const displayAllPets = (pets) => {
           </div></div>
     `;
     return;
-  }else{
+  } else {
     petContainer.classList.add("grid");
   }
 
@@ -87,7 +161,7 @@ const displayAllPets = (pets) => {
     <p class="flex items-center gap-2"><img class="w-4" src="https://img.icons8.com/?size=24&id=84997&format=png"> Birth: ${
       pet.date_of_birth ? pet.date_of_birth : "Not available"
     }</p>
-    <p class="flex items-center gap-2"><img class="w-4" src="https://img.icons8.com/?size=25&id=2LHzVg3k4AqG&format=png"> Gender: ${
+    <p class="flex items-center gap-2"><img class="w-4" src="https://img.icons8.com/?size=32&id=16275&format=png"> Gender: ${
       pet.gender ? pet.gender : "Not available"
     }</p>
     <p class="flex items-center gap-2"><img class="w-4" src="https://img.icons8.com/?size=24&id=85782&format=png"> Price: ${
@@ -96,9 +170,13 @@ const displayAllPets = (pets) => {
   </div>
   <hr>
   <div class="card-actions flex justify-between mt-3">
-      <button class="border border-gray-400 rounded-lg p-2 hover:bg-gray-300"><img src="https://img.icons8.com/?size=24&id=82788&format=png"></button>
-      <button class="font-bold text-lg text-[#0E7A81] border border-gray-400 rounded-lg p-2 hover:bg-gray-300">Adopt</button>
-      <button class="font-bold text-lg text-[#0E7A81] border border-gray-400 rounded-lg p-2 hover:bg-gray-300">Details</button>
+      <button onclick="loadPetPhotos(${
+        pet.petId
+      })" class="border border-gray-400 rounded-lg p-2 hover:bg-gray-300"><img src="https://img.icons8.com/?size=24&id=82788&format=png"></button>
+      <button onclick="openModal()" class="font-bold text-lg text-[#0E7A81] border border-gray-400 rounded-lg p-2 hover:bg-gray-300">Adopt</button>
+      <button onclick="loadPetDetails(${
+        pet.petId
+      })" class="font-bold text-lg text-[#0E7A81] border border-gray-400 rounded-lg p-2 hover:bg-gray-300">Details</button>
     </div>
   </div>
         `;
